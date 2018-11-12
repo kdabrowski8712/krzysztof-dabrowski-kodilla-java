e14bed4f492e50e904422f9ba86ff1842731ace3
kdabrowski8712 <k.dabrowski8712@gmail.com>
2018-11-12 18:26:04 +0100

    Calculator and testing Class Added

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        System.out.println("Klasa Calculator - test jednostkowy");
+
+        Calculator testCalculator = new Calculator();
+
+        int a = 5;
+        int b = 10;
+
+        if(testCalculator.add(a,b)==15) {
+            System.out.println("Add method with parameters " + a + " and " + b + " returned proper value  - TEST OK" );
+        }
+        else {
+            System.out.println("Add method with parameters " + a + " and " + b + " returned wrong value - TEST FAILED" );
+        }
+
+        if(testCalculator.subtract(a,b)==5) {
+            System.out.println("Subtract method with parameters " + a + " and " + b + " returned proper value - TEST OK" );
+        }
+        else {
+            System.out.println("Subtract method with parameters " + a + " and " + b + "returned wrong value - TEST FAILED" );
+        }
     }
 }
